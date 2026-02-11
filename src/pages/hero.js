66a70585c24e8030f70fd3e622ab5fb6f14a3111
bemndy <brandonprvbox@gmail.com>
2026-02-11 16:57:07 -0500

    hero page formatting + projects array

--- src/pages/hero.js
+++ src/pages/hero.js
@@ -5,44 +5,59 @@ import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import './hero.css'
 
-
+// initializating smooth scroll
 gsap.registerPlugin(ScrollTrigger)
 
+// TODO: make this exported via json, ordered not mattering (uses forEach callback rightnow)
 const projects = [
+  {  
+    title: "Cozy Cubes",
+    source: "c2fy.png",
+    color: "blue",
+    category: "Fullstack",
+    date: "JAN 2026"
+  },
   {
-    title: "Futbol Detection",
+    title: "Futbol Object-Detection",
     source: "sent.png",
     color: "white",
     category: "Computer Vision",
     date: "APR 2025"
   },
-  {  
-    title: "Spotify Dev Tools",
-    source: "c2fy.png",
-    color: "blue",
-    category: "Fullstack",
-    date: "MAR 2025"
-  },
-  {
-    title: "Pose Estimation",
+  { 
+    title: "OCV Pose Estimation",
     source: "pest.png",
     color: "red",
     category: "Computer Vision",
     date: "DEC 2024"
   },
   {
-    title: "Conway Console",
+    title: "Flappy Bird Terminal",
     source: "sent.png",
     color: "white",
-    category: "Backend",
+    category: "CLI",
+    date: "DEC 2024"
+  },
+  {
+    title: "Game of Life Terminal",
+    source: "sent.png",
+    color: "white",
+    category: "CLI",
     date: "NOV 2024"
   },
   {
-    title: "Sentimental Analysis",
+    title: "Sentiment Analysis",
     source: "sent.png",
     color: "white",
-    category: "Data",
-    date: "OCT 2024"
+    category: "Machine Learning",
+    date: "SEP 2024"
+  },
+  {
+    title: "Autonomous Robot",
+    source: "sent.png",
+    color: "black",
+    category: "Machine Learning",
+    date: "APR 2024"
   }
 ]
 
@@ -127,50 +142,50 @@ function Hero () {
         );
       }, []);       
     return (
-            <div className='hero-wrapper'>
-                <div className='gradient-bg'>
-                    <svg xmlns="http://www.w3.org/2000/svg">
-                        <defs>
-                            <filter id="goo">
-                            <feGaussianBlur in="SourceGraphic" stdDeviation="25" result="blur" />
-                            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo" />
-                            <feBlend in="SourceGraphic" in2="goo" />
-                            </filter>
-                        </defs>
-                    </svg>
-                    <div className='gradient-container'>
-                        {/* <div className='g1'></div>
-                        <div className="g2"></div>
-                        <div className="g3"></div>
-                        <div className='g4'></div>
-                        <div className="g5"></div> */}
-                        <div className="interactive"></div>
-                    </div>
-                </div>
-                <div className="intro-container">
-                    <h1 className="greeting-header">Brandon E Martinez</h1>
-                    <h1 className="greeting-header" id="greeting-pixel">cs @ notredame</h1>
-                    <h1 className="greeting-header">Based in Philadelphia</h1>
-                    <h1 className="greeting-header">BEM&copy;2026</h1>
-                    <div className="typewriter-container"><span className="typewriter-text"></span>
-                </div>
-                </div>
-                <Header className="header"/>
-                <div className='projects-container'>
-                  <div className='project-header'>
-                    <div className='projects-header'>project</div>
-                    <div className='projects-header'>category</div>
-                    <div className='projects-header'>date</div>
-                  </div>
-                  {
-                    projects.map( (project, index) => { 
-                      return <Project key={index} index={index} title={project.title} category={project.category}
-                      date = {project.date} setModal={setModal}/>
-                    })
-                  }   
-                  <div className='space'></div>
-                </div>
+      <div className='hero-wrapper'>
+        <div className='gradient-bg'>
+          <svg xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                  <filter id="goo">
+                  <feGaussianBlur in="SourceGraphic" stdDeviation="25" result="blur" />
+                  <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo" />
+                  <feBlend in="SourceGraphic" in2="goo" />
+                  </filter>
+              </defs>
+          </svg>
+          <div className='gradient-container'>
+              {/* <div className='g1'></div>
+              <div className="g2"></div>
+              <div className="g3"></div>
+              <div className='g4'></div>
+              <div className="g5"></div> */}
+              <div className="interactive"></div>
+          </div>
+        </div>
+          <div className="intro-container">
+              <h1 className="greeting-header">Brandon E Martinez</h1>
+              <h1 className="greeting-header" id="greeting-pixel">cs @ notredame</h1>
+              <h1 className="greeting-header">Based in Philadelphia</h1>
+              <h1 className="greeting-header">BEM&copy;2026</h1>
+              <div className="typewriter-container"><span className="typewriter-text"></span>
+          </div>
+          </div>
+          <Header className="header"/>
+          <div className='projects-container'>
+            <div className='project-header'>
+              <div className='projects-header'>project</div>
+              <div className='projects-header'>category</div>
+              <div className='projects-header'>date</div>
             </div>
+            {
+              projects.map( (project, index) => { 
+                return <Project key={index} index={index} title={project.title} category={project.category}
+                date = {project.date} setModal={setModal}/>
+              })
+            }   
+            <div className='space'></div>
+          </div>
+      </div>
     )
 };
 export default Hero
