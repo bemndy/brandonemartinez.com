27f312e4c2e6716da743f027e873ca14f606de01
bemndy <brandonprvbox@gmail.com>
2026-03-07 00:17:22 -0500

    these changes are for a useless component, being kept in case, and name changed and imports

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import './project.css'
+import './Project.css'
 
-export default function project({index, title, category, date, setModal}) {
+export default function Project({index, title, category, date, setModal}) {
   return (
       <div className="project" onMouseEnter={() => {setModal({active: true, index: index})}}
       onMouseLeave={() => {setModal({active: false, index: index})}}>
           <div className="project-title">{title}</div>
-          <div className="project-category">{category}</div>
           <div className="project-date">{date}</div>
       </div>
   )
