a446cfd11b1cf8bc02add0ea8f22e14bb527cff4
bemndy <brandonprvbox@gmail.com>
2026-08-03 21:43:47 -0400

    fix: removed unnecssary components from 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,18 +1,11 @@
-import { Link } from 'react-router-dom';
-import { FlipLink } from '../components/LinkWrap/LinkWrap';
 import './NotFound.css'
 
 function NotFound () {
     return (
-        <>
         <div className="not-found-wrapper">
             <div className="not-found-code">404</div>
             <div className="not-found-message">this page doesn't exist</div>
-            <Link to="/" className="not-found-link">
-                <FlipLink>back home</FlipLink>
-            </Link>
         </div>
-        </>
     )
 };
 export default NotFound
